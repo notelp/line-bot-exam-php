dfc511e184e6e9a5f37d2fb8cdd49413237a479c
notelp <53460446+notelp@users.noreply.github.com>
2019-07-31 10:24:09 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '0nNu0xRS7KEl5GJzWa73QXr5mzQQbyoIbM4MFcmhnUbFnPwr5Tm+HDQDmn8C8Qe0fxDQ46qzQes5LShvG4MrYJFoYCAJ+BF0RwplirGCzCpSh1S2A/TjSx4yzIGCSFB1s0/Lfi3V3fiz+ySwKeQqKQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'f44115334f1356dcdd3c18d451c9c3f0';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Ub61eee74f14a06d62b99342d790855b6';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
